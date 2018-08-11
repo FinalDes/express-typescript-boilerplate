ddef290528e34337bee69e2fab3b197139b73ab8
Alex Alonso <alex4551@gmail.com>
2018-08-11 16:05:05 +0800

    express init typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-console */
+import express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3100, () => {
+    console.log('Example app listening on port 3000!');
+});
